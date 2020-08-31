dc9c3136fd6b6d8150cfd72535bd1c4bedb98ee9
Florian Guitton <florian@secretarium.com>
2020-08-31 10:42:22 +0100

    chore: Remove Apollo client code

--- packages/mobile/src/Providers.tsx
+++ packages/mobile/src/Providers.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { ApolloProvider } from '@apollo/client';
 import { Router } from './react-router';
-import { apolloClient } from './apollo';
 import Navigation from './Navigation';
 
 const Providers: React.FC = () => (
-    <ApolloProvider client={apolloClient}>
-        <Router>
-            <Navigation />
-        </Router>
-    </ApolloProvider>
+    <Router>
+        <Navigation />
+    </Router>
 );
 
 export default Providers;
