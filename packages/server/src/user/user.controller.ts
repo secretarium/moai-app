692296f76c53b656118b1af9f8d471b2ca0dcc51
Florian Guitton <florian@secretarium.com>
2020-09-04 18:04:51 +0100

    chore: Cleaning up tags from User controller

--- packages/server/src/user/user.controller.ts
+++ packages/server/src/user/user.controller.ts
@@ -1,5 +1,4 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import NotAuthorizedException from '../exceptions/NotAuthorizedException';
 import authMiddleware from '../middleware/auth.middleware';
 import userModel from './user.model';
 import UserNotFoundException from '../exceptions/UserNotFoundException';
@@ -15,30 +14,17 @@ class UserController implements Moai.Controller {
 
     private initializeRoutes() {
         this.router.get(`${this.path}/:id`, authMiddleware, this.getUserById);
-        this.router.get(`${this.path}/:id/tags`, authMiddleware, this.getAllTagsOfUser);
     }
 
     private getUserById = async (request: Request, response: Response, next: NextFunction) => {
         const id = request.params.id;
         const userQuery = this.user.findById(id);
-        if (request.query.withTags === 'true') {
-            userQuery.populate('tags').exec();
-        }
         const user = await userQuery;
         if (user) {
             response.json(user);
         } else {
             next(new UserNotFoundException(id));
         }
-    }
-
-    private getAllTagsOfUser = async (request: Moai.RequestWithUser, response: Response, next: NextFunction) => {
-        const userId = request.params.id;
-        if (userId === request.user._id.toString()) {
-            const tags = await this.tag.find({ author: userId });
-            response.json(tags);
-        }
-        next(new NotAuthorizedException());
     }
 }
 
