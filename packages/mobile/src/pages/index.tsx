da0bd981bbcaabdf3c9a60109ab29805d01da43c
Florian Guitton <florian@secretarium.com>
2020-09-16 16:11:06 +0100

    chore: Removing i18n from index page

--- packages/mobile/src/pages/index.tsx
+++ packages/mobile/src/pages/index.tsx
@@ -1,25 +1,14 @@
 import Banner from 'components/Website/Banner';
 import { NextPage } from 'next';
 import React from 'react';
-import NextI18N from '../i18n';
-
-const { useTranslation } = NextI18N;
 
 const IndexPage: NextPage = () => {
-
-    const { t } = useTranslation();
 
     return (
         <div>
             <Banner />
         </div>
     );
-};
-
-IndexPage.getInitialProps = () => {
-    return {
-        namespacesRequired: ['common']
-    };
 };
 
 export default IndexPage;
