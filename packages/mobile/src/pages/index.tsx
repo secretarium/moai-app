7487a675871d4c29c8906236d8c099f813c1362d
Florian Guitton <florian@secretarium.com>
2020-09-16 11:05:07 +0100

    chore: Fixing unused variable linting errors

--- packages/mobile/src/pages/index.tsx
+++ packages/mobile/src/pages/index.tsx
@@ -1,21 +1,9 @@
 import Banner from 'components/Website/Banner';
 import { NextPage } from 'next';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import NextI18N from '../i18n';
 
-const { useTranslation, i18n, Link } = NextI18N;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    text: {
-        fontSize: 16
-    }
-});
+const { useTranslation } = NextI18N;
 
 const IndexPage: NextPage = () => {
 
@@ -25,16 +13,6 @@ const IndexPage: NextPage = () => {
         <div>
             <Banner />
         </div>
-        // <View style={styles.container}>
-        //     <Text style={styles.text}>{t('hello')} <span aria-label="Waving hand" role="img">👋</span></Text>
-        //     <button
-        //         type='button'
-        //         onClick={() => i18n.changeLanguage(i18n.language === 'en' ? 'fr' : 'en')}
-        //     >
-        //         {t('change-locale')}
-        //     </button>
-        //     <Link href='/check' >Check</Link>
-        // </View>
     );
 };
 
