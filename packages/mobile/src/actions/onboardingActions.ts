2eba95b6e395108c3b7f923d2e256f9448a2d152
Florian Guitton <florian@secretarium.com>
2020-09-22 11:35:15 +0100

    chore: cleaning up onboarding reducer

--- packages/mobile/src/actions/onboardingActions.ts
+++
@@ -1,8 +0,0 @@
-import { SHOW_ONBOARDING } from './types';
-
-export interface ShowOnboarding {
-    type: typeof SHOW_ONBOARDING;
-    payload: boolean;
-}
-
-export type OnboardingActions = ShowOnboarding;
