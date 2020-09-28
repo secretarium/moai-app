11082d72a7067a793a24a9bb85077a5cab9676b5
Florian Guitton <florian@secretarium.com>
2020-09-28 15:29:55 +0100

    feat: Adding support for Moai and NHS scanning

--- packages/mobile/src/store/index.ts
+++ packages/mobile/src/store/index.ts
@@ -19,7 +19,9 @@ const persistConfig = {
 };
 
 const isDev = process.env.NODE_ENV === 'development';
-const middlewares = isDev ? [dispatchMiddleware, createLogger()] : [dispatchMiddleware];
+const middlewares = isDev ? [dispatchMiddleware, createLogger({
+    collapsed: true
+})] : [dispatchMiddleware];
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const immutableStateReducer = (state: any, ownProps: any) => fromJS(persistedReducer(state?.toJS ? state.toJS() : state, ownProps));
 
