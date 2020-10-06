c7fa7b6a0c93754dba369904ced5ae36d8a9ce53
Florian Guitton <florian@secretarium.com>
2020-10-06 09:35:18 +0100

    chore: Re-enable appstate change side effect

--- packages/mobile/src/App.native.tsx
+++ packages/mobile/src/App.native.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import { withState } from './store';
 import * as Linking from 'expo-linking';
 import { Redirect, Route, Switch } from './ReactRouter';
@@ -14,9 +14,8 @@ import Licenses from './components/Infos/Licenses';
 import Infos from './components/Infos';
 import OnboardingScreen from './components/Onboarding/OnboardingScreen';
 import { generateLocalKey } from './actions';
-import { useFonts } from 'expo-font';
 import { styles } from './styles';
-import { AppState, View, Image } from 'react-native';
+import { AppState, View, Image, AppStateStatus } from 'react-native';
 import { useHistory } from 'react-router';
 
 const App = withState()(
@@ -26,18 +25,13 @@ const App = withState()(
     ({ dispatch, localKey }) => {
 
         const history = useHistory();
+        const appState = useRef(AppState.currentState);
         const [initialUrl, setInitialUrl] = useState<string>(undefined);
         const [pastInitialUrl, setPastInitialUrl] = useState<string>(undefined);
         const [hasRequestedInitialURL, setHasRequestedInitialURL] = useState(false);
         const [hasParsedInitialURL, setHasParsedInitialURL] = useState(true);
         const [hasRequestedLocalKey, setHasRequestedLocalKey] = useState(false);
         const [hasObtainedLocalKey, setHasObtainedLocalKey] = useState(false);
-        // const [hasPluggedStateChange, setHasPluggedStateChange] = useState(false);
-
-        // const [fontsLoaded] = useFonts({
-        //     'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
-        //     'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf')
-        // });
 
         const parseUrl = useCallback((url: string | null | undefined) => {
 
@@ -83,21 +77,24 @@ const App = withState()(
             }
         }, [dispatch, hasRequestedLocalKey, localKey]);
 
-        // const handleAppStateChange = useCallback((nextAppState: string) => {
-        //     if (nextAppState === 'active') {
-        //         setHasRequestedInitialURL(false);
-        //         if (!initialUrl && history.location.pathname.slice(0, 8) === '/scanner') {
-        //             history.push('/');
-        //         }
-        //     }
-        // }, [history, initialUrl]);
+        const handleAppStateChange = (nextAppState: AppStateStatus) => {
+            if (appState.current.match(/inactive|background/) && nextAppState === 'active') {
+                setInitialUrl(undefined);
+                setHasParsedInitialURL(false);
+                setHasRequestedInitialURL(false);
+                // if (!initialUrl && history.location.pathname.slice(0, 8) === '/scanner') {
+                //     history.push('/');
+                // }
+            }
+            appState.current = nextAppState;
+        };
 
-        // useEffect(() => {
-        //     if (!hasPluggedStateChange) {
-        //         AppState.addEventListener('change', handleAppStateChange);
-        //         setHasPluggedStateChange(true);
-        //     }
-        // }, [handleAppStateChange, hasPluggedStateChange]);
+        useEffect(() => {
+            AppState.addEventListener('change', handleAppStateChange);
+            return () => {
+                AppState.removeEventListener('change', handleAppStateChange);
+            };
+        }, []);
 
         if (!hasObtainedLocalKey || !hasParsedInitialURL)
             return <View style={styles.container}>
