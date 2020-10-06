0d11cc21fafac801ab0420d0fec19b7eb95e343d
Florian Guitton <florian@secretarium.com>
2020-10-06 11:45:57 +0100

    chore: v0.0.10

--- packages/mobile/app.config.js
+++ packages/mobile/app.config.js
@@ -1,7 +1,7 @@
 const config = {
     name: 'Moai',
     description: 'An app for Confidential Track and Trace',
-    version: '0.0.9',
+    version: '0.0.10',
     slug: 'moai-confidential-test-and-trace',
     icon: './assets/icon.png',
     splash: {
@@ -12,7 +12,7 @@ const config = {
     userInterfaceStyle: 'automatic',
     orientation: 'portrait',
     ios: {
-        buildNumber: '9',
+        buildNumber: '10',
         icon: './assets/ios/icon.png',
         bundleIdentifier: 'com.secretarium.moai.app',
         associatedDomains: [
@@ -21,7 +21,7 @@ const config = {
         ]
     },
     android: {
-        versionCode: 9,
+        versionCode: 10,
         icon: './assets/android/icon.png',
         package: 'com.secretarium.moai.app',
         permissions: [
