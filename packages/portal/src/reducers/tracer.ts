439562210e5605135109e527b1f74553d72252b0
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2020-12-14 00:15:48 +0000

    fix: Removed margin-left from main components

--- packages/portal/src/reducers/tracer.ts
+++ packages/portal/src/reducers/tracer.ts
@@ -2,7 +2,7 @@ import { Tracer, StoreComponent } from '../global';
 import { actionTypes, commands } from '../actions/constants';
 
 export const initialState: Tracer = {
-    isConnected: true,  // CHANGE
+    isConnected: false,
     isVerified: null
 };
 
