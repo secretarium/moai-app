d5f3d593fce5d426fac29a7d37d9c13c10eafb26
Florian Guitton <florian@secretarium.com>
2020-12-17 23:25:48 +0100

    chore: Moving to using local FontAwesome assets

--- packages/portal/src/components/Menu/Menu.tsx
+++ packages/portal/src/components/Menu/Menu.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './Menu.css';
 import { NavLink } from 'react-router-dom';
 import Logout from '../../assets/logout.png';
 import Logo from '../../assets/logo-white.png';
 import { disconnect } from '../../actions';
 import { useDispatch } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faComment, faBarcode, faQrcode, faInfo } from '@fortawesome/free-solid-svg-icons';
+import style from './Menu.module.css';
 
 
 const Menu: React.FC = () => {
@@ -12,38 +14,38 @@ const Menu: React.FC = () => {
     const dispatch = useDispatch();
 
     return (
-        <div className="sidebar">
-            <div className="header">
+        <div className={style.sidebar}>
+            <div className={style.header}>
                 {/** connection status */}
                 <img src={Logo} alt="moai" style={{ width: '100px', height: 'auto' }} />
             </div>
-            <div className="options">
-                <NavLink exact className="option" to={'/'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <i className="fas fa-home fa-lg" style={{ paddingRight: '10px' }} />
+            <div className={style.options}>
+                <NavLink exact className={style.option} to={'/'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
+                    <FontAwesomeIcon icon={faHome} />
                     Home
                 </NavLink>
-                <NavLink className="option" to={'/chat'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <i className="fas fa-comments fa-lg" style={{ paddingRight: '10px' }} />
+                <NavLink className={style.option} to={'/chat'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
+                    <FontAwesomeIcon icon={faComment} />
                     Chat
                 </NavLink>
-                <NavLink className="option" to={'/search/tested'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <i className="fas fa-barcode fa-lg" style={{ paddingRight: '10px' }} />
+                <NavLink className={style.option} to={'/search/tested'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
+                    <FontAwesomeIcon icon={faBarcode} />
                     Search Test ID
                 </NavLink>
-                <NavLink className="option" to={'/search/exposed'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <i className="fas fa-qrcode fa-lg" style={{ paddingRight: '13px' }} />
+                <NavLink className={style.option} to={'/search/exposed'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
+                    <FontAwesomeIcon icon={faQrcode} />
                     Search Location Code
                 </NavLink>
-                {/* <NavLink className="option" to={'/settings'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <i className="fas fa-cog fa-lg" style={{ paddingRight: '10px' }} />
+                {/* <NavLink className={style.option} to={'/settings'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
+                    <FontAwesomeIcon icon={faCog} />
                     Settings
                 </NavLink> */}
-                <NavLink className="option" to={'/about'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <i className="fas fa-info-circle fa-lg" style={{ paddingRight: '10px' }} />
+                <NavLink className={style.option} to={'/about'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
+                    <FontAwesomeIcon icon={faInfo} />
                     About Moai
                 </NavLink>
             </div>
-            <NavLink className="logout" to={'/'} onClick={() => dispatch(disconnect())} style={{ color: '#fff' }}>
+            <NavLink className={style.logout} to={'/'} onClick={() => dispatch(disconnect())} style={{ color: '#fff' }}>
                 <img src={Logout} alt="logout" style={{ width: '45px', height: 'auto', marginRight: '8px' }} /> Logout
             </NavLink>
         </div>
