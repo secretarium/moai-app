13af5d351aefce171a0bdb1401d7befd124be2f9
Florian Guitton <florian@secretarium.com>
2020-12-19 11:55:08 +0100

    chore: Removing NextJS integration as website moved to its own repo

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -2,7 +2,6 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: [
-            '@expo/next-adapter/babel',
             ['@babel/preset-env', { targets: { node: 'current' } }],
             '@babel/preset-typescript',
             'babel-preset-expo',
