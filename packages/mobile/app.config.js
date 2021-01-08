9eeeb4d7f3a915f70eae323e107d184976e4d4db
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2021-01-08 20:52:01 +0100

    chore: [mobile] v0.0.18

--- packages/mobile/app.config.js
+++ packages/mobile/app.config.js
@@ -1,7 +1,7 @@
 const config = {
     name: 'Moai',
     description: 'An app for Confidential Track and Trace',
-    version: '0.0.17',
+    version: '0.0.18',
     slug: 'moai-confidential-test-and-trace',
     icon: './assets/icon.png',
     splash: {
@@ -12,7 +12,7 @@ const config = {
     userInterfaceStyle: 'automatic',
     orientation: 'portrait',
     ios: {
-        buildNumber: '17',
+        buildNumber: '18',
         icon: './assets/ios/icon.png',
         bundleIdentifier: 'com.secretarium.moai.app',
         associatedDomains: [
@@ -21,7 +21,7 @@ const config = {
         ]
     },
     android: {
-        versionCode: 17,
+        versionCode: 18,
         icon: './assets/android/icon.png',
         package: 'com.secretarium.moai.app',
         permissions: [
