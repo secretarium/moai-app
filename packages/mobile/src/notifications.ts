b04f46bf451d4fe512aa9ff3394b2e3d466485b5
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2021-01-13 15:52:13 +0000

    chore: Started implementing expo-notifications for the mobile app

---
+++ packages/mobile/src/notifications.ts
@@ -0,0 +1,46 @@
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import { Platform } from 'react-native';
+
+
+export const sendPushNotification = async (): Promise<void> => {
+    await Notifications.scheduleNotificationAsync({
+        content: {
+            title: 'You\'ve got mail! 📬',
+            body: 'Here is the notification body',
+            data: { data: 'goes here' }
+        },
+        trigger: { seconds: 1 }
+    });
+};
+
+export const registerForPushNotificationsAsync = async (): Promise<string> => {
+    let token;
+    if (Constants.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+    } else {
+        alert('Must use physical device for Push Notifications');
+    }
+
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C'
+        });
+    }
+
+    return token;
+};
