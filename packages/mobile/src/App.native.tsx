acce7cdaa7a43b067528a6ad4236e8267bc4e1eb
Damian Tziamtzis <damian.tziamtzis@gmail.com>
2021-01-15 12:00:11 +0000

    chore: Removed notification references from this branch

--- packages/mobile/src/App.native.tsx
+++ packages/mobile/src/App.native.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback, useRef } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { withState } from './store';
 import * as Linking from 'expo-linking';
 import { Redirect, Route, Switch } from './ReactRouter';
@@ -22,16 +22,6 @@ import { useFonts } from 'expo-font';
 import { styles } from './styles';
 import { AppState, View, Image } from 'react-native';
 import { useHistory } from 'react-router';
-import { registerForPushNotificationsAsync } from './notifications';
-import * as Notifications from 'expo-notifications';
-
-Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-        shouldShowAlert: true,
-        shouldPlaySound: false,
-        shouldSetBadge: false
-    })
-});
 
 const App = withState()(
     (s) => ({
@@ -50,10 +40,6 @@ const App = withState()(
         const [hasRequestedLocalKey, setHasRequestedLocalKey] = useState(false);
         const [hasObtainedLocalKey, setHasObtainedLocalKey] = useState(false);
         const [hasPluggedStateChange, setHasPluggedStateChange] = useState(false);
-        const [expoPushToken, setExpoPushToken] = useState('');
-        const [notification, setNotification] = useState<Notifications.Notification>();
-        const notificationListener = useRef<any>();
-        const responseListener = useRef<any>();
 
         const [fontsLoaded] = useFonts({
             'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
@@ -67,19 +53,6 @@ const App = withState()(
                 setInitialUrl(comps[1]);
             else
                 setInitialUrl(null);
-        }, []);
-
-        useEffect(() => {
-            registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-
-            notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-                setNotification(notification);
-            });
-
-            responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-                console.log(response);
-            });
-
         }, []);
 
         useEffect(() => {
