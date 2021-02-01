f4a2c0db165216a188e51c38554643f405143611
damtzi <damian.tziamtzis@gmail.com>
2021-02-01 11:45:35 +0000

    chore: Changed bundleId and remove 'tracing' keywords

--- packages/mobile/src/components/Infos/Infos.tsx
+++ packages/mobile/src/components/Infos/Infos.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, ScrollView } from 'react-native';
 import { useColorScheme } from 'react-native-appearance';
 import { Entypo } from '@expo/vector-icons';
 import { Link } from '../../ReactRouter';
@@ -24,24 +24,26 @@ const Infos: React.FC = () => {
                     {i18n.t('APP_ABOUT')}
                 </Text>
                 <Text style={{ fontFamily: 'Poppins-Regular', color: themeTextStyle }}>
-                    Moai is a smart, secure contact tracing solution created by Secretarium, a deep-tech startup founded in 2016 with the aim of building useful technology that never compromises anyone’s privacy.{'\n'}{'\n'}
+                    Moai gathers information securely via a short survey to measure factors that affect the spread of COVID-19. Developed by Secretarium, a deep-tech startup founded in 2016 with the aim of building useful technology that never compromises anyone’s privacy.{'\n'}{'\n'}
                     The founders are from the world of banking, but they left to pursue projects that would solve problems around handling highly sensitive data: enforcing consent and guaranteeing privacy by design and by default.
                 </Text>
             </View>
-            {i18n.t('APP_INFOS').map((item, index) =>
-                <TouchableOpacity style={[styles.card, { backgroundColor: themeColorStyle }]} key={index}>
-                    <Link to={`/${item.toLowerCase()}`} style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between' }} underlayColor='transparent'>
-                        <>
-                            <Text style={{ fontFamily: 'Poppins-Bold', color: themeTextStyle, fontSize: 15 }}>{item}</Text>
-                            <Entypo
-                                name="chevron-right"
-                                style={{ alignSelf: 'center' }}
-                                color={themeTextStyle}
-                                size={24} />
-                        </>
-                    </Link>
-                </TouchableOpacity>
-            )}
+            <ScrollView>
+                {i18n.t('APP_INFOS').map((item, index) =>
+                    <TouchableOpacity style={[styles.card, { backgroundColor: themeColorStyle }]} key={index}>
+                        <Link to={`/${item.toLowerCase()}`} style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between' }} underlayColor='transparent'>
+                            <>
+                                <Text style={{ fontFamily: 'Poppins-Bold', color: themeTextStyle, fontSize: 15 }}>{item}</Text>
+                                <Entypo
+                                    name="chevron-right"
+                                    style={{ alignSelf: 'center' }}
+                                    color={themeTextStyle}
+                                    size={24} />
+                            </>
+                        </Link>
+                    </TouchableOpacity>
+                )}
+            </ScrollView>
         </MainLayout>
     );
 };
