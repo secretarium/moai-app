79745f48fbb5f510959573726e2d6932aa30f665
damtzi <damian.tziamtzis@gmail.com>
2021-02-10 17:18:28 +0000

    chore: Switching FontAwesome icons to antd + adding Admin route

--- packages/portal/src/components/Menu/Menu.tsx
+++ packages/portal/src/components/Menu/Menu.tsx
@@ -3,8 +3,7 @@ import { NavLink } from 'react-router-dom';
 import Logo from '../../assets/logo-white.png';
 import { disconnect } from '../../actions';
 import { useDispatch } from 'react-redux';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faComment, faBarcode, faQrcode, faInfo, faPowerOff } from '@fortawesome/free-solid-svg-icons';
+import { MessageOutlined, HomeOutlined, QrcodeOutlined, BarcodeOutlined, TeamOutlined, InfoCircleOutlined, LogoutOutlined } from '@ant-design/icons';
 import style from './Menu.module.css';
 import { useTranslation } from 'react-i18next';
 
@@ -21,35 +20,32 @@ const Menu: React.FC = () => {
             </div>
             <div className={style.options}>
                 <NavLink exact className={style.option} to={'/'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <FontAwesomeIcon icon={faHome} />
-                    <span>{t('APP_HOME')}</span>
+                    <HomeOutlined />
+                    <span className={style.navOption}>{t('APP_HOME')}</span>
                 </NavLink>
                 <NavLink className={style.option} to={'/chat'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <FontAwesomeIcon icon={faComment} />
-                    <span>{t('APP_CHAT')}</span>
+                    <MessageOutlined />
+                    <span className={style.navOption}>{t('APP_CHAT')}</span>
                 </NavLink>
                 <NavLink className={style.option} to={'/search/tested'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <FontAwesomeIcon icon={faBarcode} />
-                    <span>{t('APP_SEARCH_TEST_ID')}</span>
+                    <BarcodeOutlined />
+                    <span className={style.navOption}>{t('APP_SEARCH_TEST_ID')}</span>
                 </NavLink>
                 <NavLink className={style.option} to={'/search/exposed'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <FontAwesomeIcon icon={faQrcode} />
-                    <span>{t('APP_SEARCH_LOCATION_CODE')}</span>
+                    <QrcodeOutlined />
+                    <span className={style.navOption}>{t('APP_SEARCH_LOCATION_CODE')}</span>
                 </NavLink>
-                {/* <NavLink className={style.option} to={'/settings'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <FontAwesomeIcon icon={faCog}  />
-                    <span>Settings</span>
-                </NavLink> */}
+                <NavLink className={style.option} to={'/admin'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
+                    <TeamOutlined />
+                    <span className={style.navOption}>{t('APP_ADMIN')}</span>
+                </NavLink>
                 <NavLink className={style.option} to={'/about'} style={{ color: '#8B8C9D' }} activeStyle={{ color: '#FAFCFC' }}>
-                    <FontAwesomeIcon icon={faInfo} style={{ width: '0.8rem' }} />
-                    <span>{t('APP_ABOUT_MOAI')}</span >
+                    <InfoCircleOutlined />
+                    <span className={style.navOption}>{t('APP_ABOUT_MOAI')}</span>
                 </NavLink>
-                <br />
-                <br />
-                <br />
-                <NavLink className={style.option} to={'/'} onClick={() => dispatch(disconnect())} >
-                    <FontAwesomeIcon icon={faPowerOff} />
-                    <span>{t('APP_LOGOUT')}</span >
+                <NavLink className={style.option} to={'/'} onClick={() => dispatch(disconnect())}>
+                    <LogoutOutlined />
+                    <span className={style.navOption}>{t('APP_LOGOUT')}</span>
                 </NavLink>
             </div>
         </div>
