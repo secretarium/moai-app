733991de128a224797e925d55132ddf0d1704f03
damtzi <damian.tziamtzis@gmail.com>
2021-04-13 16:19:03 +0100

    chore: Fixed notificiation message

--- packages/mobile/src/components/Notification/Notification.tsx
+++ packages/mobile/src/components/Notification/Notification.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
 import MainLayout from '../common/MainLayout';
 import { useColorScheme } from 'react-native-appearance';
 import { RouteComponentProps } from 'react-router';
 import i18n from 'i18n-js';
 import { withState } from '../../store';
-
+import { useHistory } from 'react-router';
+import { commonStyles } from '../commonStyles';
 
 type NotificationProps = RouteComponentProps<{
     notificationMessage: string;
@@ -16,10 +17,20 @@ const Notification = withState<NotificationProps>()(
     ({ match }) => {
 
         const { params: { notificationMessage } } = match;
+        const history = useHistory();
+        const [notifContent, setNotifContent] = useState();
 
         // Color theme
         const colorScheme = useColorScheme();
         const themeTextStyle = colorScheme !== 'dark' ? 'black' : 'white';
+        const themeColorStyle = colorScheme !== 'dark' ? '#D3D3D3' : '#404040';
+
+        useEffect(() => {
+            if (notificationMessage) {
+                const notifObj = JSON.parse(notificationMessage);
+                setNotifContent(notifObj.type);
+            }
+        })
 
         return (
             <MainLayout goBackRoute={'/'} showGoBack={true}>
@@ -30,9 +41,18 @@ const Notification = withState<NotificationProps>()(
                     <Text style={{ fontFamily: 'Poppins-Bold', color: themeTextStyle, fontSize: 25, paddingBottom: 15 }}>
                         {i18n.t('APP_NOTIFICATION_ALERT')}
                     </Text>
-                    <Text style={{ fontFamily: 'Poppins-Regular', color: themeTextStyle }}>
-                        {notificationMessage}
-                    </Text>
+                    {(notifContent === 'feedbackInvitation')
+                        ? <>
+                            <Text style={{ fontFamily: 'Poppins-Regular', color: themeTextStyle }}>Would you like to answer a few questions to learn more about your exposure?</Text>
+                            <View style={[commonStyles.homeButtonContainer, { marginTop: 80 }]} >
+                                <TouchableOpacity onPress={() => history.push('/feedback/form')} style={[commonStyles.homeButton, { backgroundColor: themeColorStyle, marginBottom: 15 }]}>
+                                    <Text style={{ fontFamily: 'Poppins-Bold', color: themeTextStyle }}>Yes</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => history.push('/')} style={[commonStyles.homeButton, { backgroundColor: themeColorStyle }]}>
+                                    <Text style={{ fontFamily: 'Poppins-Bold', color: themeTextStyle }}>No</Text>
+                                </TouchableOpacity>
+                            </View></>
+                        : <Text style={{ fontFamily: 'Poppins-Regular', color: themeTextStyle }}>{notificationMessage}</Text>}
                 </View>
             </MainLayout>
         );
