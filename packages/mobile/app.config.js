25d1bd87b4ad9d6ba81cb8e28b27b7ff48b8ee6e
damtzi <damian.tziamtzis@gmail.com>
2021-04-13 16:20:49 +0100

    chore: [mobile] v0.0.38

--- packages/mobile/app.config.js
+++ packages/mobile/app.config.js
@@ -1,7 +1,7 @@
 const config = {
     name: 'Moai',
     description: 'A research app developed by Secretarium',
-    version: '0.0.37',
+    version: '0.0.38',
     slug: 'moai-app-research',
     icon: './assets/icon.png',
     splash: {
@@ -12,7 +12,7 @@ const config = {
     userInterfaceStyle: 'automatic',
     orientation: 'portrait',
     ios: {
-        buildNumber: '40',
+        buildNumber: '41',
         icon: './assets/ios/icon.png',
         bundleIdentifier: 'com.secretarium.moai.research',
         associatedDomains: [
@@ -24,7 +24,7 @@ const config = {
         }
     },
     android: {
-        versionCode: 38,
+        versionCode: 39,
         icon: './assets/android/icon.png',
         package: 'com.secretarium.moai.research',
         useNextNotificationsApi: true,
